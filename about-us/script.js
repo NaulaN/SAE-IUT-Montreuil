9a9d0f096c2aa794714feb6ee02725792a7ec950
nonoc <nonochrzaszcz@gmail.com>
2021-12-18 19:47:24 +0100

    Little correction

--- about-us/script.js
+++ about-us/script.js
@@ -4,20 +4,31 @@
 let presentationOfUITOfMontreuilSection = document.getElementById("presentation-of-UIT-div");
 let digitalFingerprintSection = document.getElementById("digital-fingerprint-div");
 
+let littleNavButton0 = document.getElementById("little-nav-button-0");
+let littleNavButton1 = document.getElementById("little-nav-button-1");
 
-function showPresentationOfUit() {
+
+function showPresentationOfUit(ev) {
     presentationOfUITOfMontreuilSection.style.display = "inherit";
+    littleNavButton0.style.backgroundColor = "#fba292";
+    littleNavButton0.style.border = "solid 5px #fba292";
     digitalFingerprintSection.style.display = "none";
+    littleNavButton1.style.backgroundColor = "#4e5a65";
+    littleNavButton1.style.border = "solid 5px #4e5a65";
 }
 
-function showdigitalFingerPrint() {
+function showdigitalFingerPrint(ev) {
     presentationOfUITOfMontreuilSection.style.display = "none";
+    littleNavButton0.style.backgroundColor = "#4e5a65";
+    littleNavButton0.style.border = "solid 5px #4e5a65";
     digitalFingerprintSection.style.display = "inherit";
+    littleNavButton1.style.backgroundColor = "#fba292";
+    littleNavButton1.style.border = "solid 5px #fba292";
 }
 
 
-document.getElementById("little-nav-button-0").onclick = function() {showPresentationOfUit()};
+littleNavButton0.onclick = function(ev) {showPresentationOfUit(ev)};
 document.getElementById("sommaire-part-presentation-of-UIT").onclick = function() {showPresentationOfUit()};
-document.getElementById("little-nav-button-1").onclick = function() {showdigitalFingerPrint()};
+littleNavButton1.onclick = function(ev) {showdigitalFingerPrint(ev)};
 document.getElementById("sommaire-part-digitalFingerprint").onclick = function() {showdigitalFingerPrint()};
 
